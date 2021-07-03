c378628c5365b332f38260f035369b26345685d5
Nicolas Jeanmonod <nicolas.jeanmonod@gmail.com>
2021-07-03 17:46:30 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 /*
 
+CAMERA SLOW MOTION CALIBRATION
+
+# DESCRIPTION OF THE PROJECT
+
+The goal of this project is to calibrate the slow motion frame rate of a smartphone.
+14 LEDs are displaying a counter that counts from 0 to 16383 in binary.
+The counter is updated at 1 ms interval with a timer.
+
+# SETUP
+
+This project works only with ATmega328P microcontrolers (ARDUINO UNO, Nano, Micro...).
+Connect the LEDs to output 0 to 13
+
+# ATmega328P datasheet
+
+http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+
+# ARTICLE ABOUT TIMERS (in French)
+
 https://www.locoduino.org/spip.php?article84
 
-http://www.engblaze.com/microcontroller-tutorial-avr-and-arduino-timer-interrupts/
-
-PORTD1  LED front (Red)
-
-PORTD4  LED1 (Red)
-PORTD5  LED2 (Green)
-PORTD6  LED3 (Green)
-PORTD7  LED4 (Red)
-
-PORTB5  LED board (red)
 
 */
 
@@ -28,16 +37,21 @@ volatile uint8_t PORTD_NEW = 0;
  */
 ISR(TIMER1_COMPA_vect)
 {
-    bitWrite(PORTB_NEW, PORTB5, bitRead(COUNTER, 0));
-    bitWrite(PORTB_NEW, PORTB4, bitRead(COUNTER, 1));
-    bitWrite(PORTB_NEW, PORTB3, bitRead(COUNTER, 2));
-    bitWrite(PORTB_NEW, PORTB2, bitRead(COUNTER, 3));
-    bitWrite(PORTB_NEW, PORTB1, bitRead(COUNTER, 4));
-    bitWrite(PORTB_NEW, PORTB0, bitRead(COUNTER, 5));
+    bitWrite(PORTB_NEW, 5, bitRead(COUNTER, 0));
+    bitWrite(PORTB_NEW, 4, bitRead(COUNTER, 1));
+    bitWrite(PORTB_NEW, 3, bitRead(COUNTER, 2));
+    bitWrite(PORTB_NEW, 2, bitRead(COUNTER, 3));
+    bitWrite(PORTB_NEW, 1, bitRead(COUNTER, 4));
+    bitWrite(PORTB_NEW, 0, bitRead(COUNTER, 5));
 
-    bitWrite(PORTD_NEW, PORTD7, bitRead(COUNTER, 6));
-    bitWrite(PORTD_NEW, PORTD6, bitRead(COUNTER, 7));
-    bitWrite(PORTD_NEW, PORTD5, bitRead(COUNTER, 8));
+    bitWrite(PORTD_NEW, 7, bitRead(COUNTER, 6));
+    bitWrite(PORTD_NEW, 6, bitRead(COUNTER, 7));
+    bitWrite(PORTD_NEW, 5, bitRead(COUNTER, 8));
+    bitWrite(PORTD_NEW, 4, bitRead(COUNTER, 9));
+    bitWrite(PORTD_NEW, 3, bitRead(COUNTER, 10));
+    bitWrite(PORTD_NEW, 2, bitRead(COUNTER, 11));
+    bitWrite(PORTD_NEW, 1, bitRead(COUNTER, 12));
+    bitWrite(PORTD_NEW, 0, bitRead(COUNTER, 13));
 
     cli(); // Disable global interrupts.
     PORTB = PORTB_NEW;
@@ -52,13 +66,13 @@ ISR(TIMER1_COMPA_vect)
  */
 void setup()
 {
-    // All pins of PORTB and PORT set to OUTPUT.
-    DDRD = B11111111;
+    // All pins of PORTB and PORTD set to OUTPUT.
     DDRB = B11111111;
+    DDRD = B11111111;
 
     // All ON.
-    PORTD = B11111111;
     PORTB = B11111111;
+    PORTD = B11111111;
 
     /*
 
@@ -76,34 +90,32 @@ void setup()
     OCR1A = 15624
     t = 1024 / 16E6 × ( 1 + 15624 ) = 1
 
-    for t = 4 ms
+    for t = 1 ms
     CS12 CS11 CS10 = 0 0 1 => prescale factor N = 1
-    OCR1A = 63999
-    t = 1 / 16E6 × ( 1 + 63999 ) = 4E-3
+    OCR1A = 15999
+    t = 1 / 16E6 × ( 1 + 15999 ) = 1E-3
 
         t        N    OCRA1
     ========================
         4E-03    1    63999
         3E-03    1    47999
         2E-03    1    31999
-        1E-03    1    15624
+        1E-03    1    15999
      0.50E-03    1    7999
      0.25E-03    1    3999
 
     */
 
-    cli();               // Disable global interrupts.
-    TCCR1A = 0b00000000; // Set normal mode of operation.
-
+    cli();                      // Disable global interrupts.
+    TCCR1A = 0b00000000;        // Set normal mode of operation.
     TCCR1B = 0b00000000;        //
     bitWrite(TCCR1B, WGM12, 1); // Turn on CTC mode.
     bitWrite(TCCR1B, CS12, 0);  // Prescale factor N.
     bitWrite(TCCR1B, CS11, 0);  // See datasheet page 110
     bitWrite(TCCR1B, CS10, 1);  // for values of CS10, CS11 and CS12.
-
-    OCR1A = 15624;           // Set compare match register to desired timer count.
-    TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt.
-    sei();                   // Enable global interrupts.
+    OCR1A = 16666;              // Set compare match register to desired timer count.
+    TIMSK1 |= (1 << OCIE1A);    // Enable timer compare interrupt.
+    sei();                      // Enable global interrupts.
 }
 
 /**

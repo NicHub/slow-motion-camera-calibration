1a14fa24ea3430df64e8207b4a2147c8297e2c9f
Nicolas Jeanmonod <nicolas.jeanmonod@gmail.com>
2021-09-13 20:05:52 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # CAMERA SLOW MOTION CALIBRATION
+
+## ABSTRACT
+
+The goal of this project is to calibrate the slow motion frame rate of a smartphone.
+14 LEDs are displaying a counter that counts from 0 to 16383 in binary.
+The counter is updated at 1 ms interval with a timer.
+
+## SETUP
+
+This project works only with ATmega328P microcontrolers (ARDUINO UNO, Nano, Micro...).
+Connect the LEDs to output 0 to 13
+
+## ATmega328P datasheet
+
+http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+
+## ARTICLE ABOUT TIMERS (in French)
+
+https://www.locoduino.org/spip.php?article84
+
+
+## Samsung S21
+
+https://www.androidpolice.com/2021/01/20/samsungs-smaller-cheaper-s21-and-s21-beat-the-ultra-at-slow-mo-video/
+
+https://www.samsung.com/global/galaxy/galaxy-s21-5g/specs/
+
+Super Slow-mo only supports HD reans. On Galaxy S21 5G and S21+ 5G, users can record approximately 0.5 seconds of video captured at 960 fps with approximately 16 seconds of playback. On Galaxy S21 Ultra 5G, users can record approximately 1 second of video captured at 480 fps and digitally enhance the video to 960 fps with approximately 32 seconds of playback. Playback time can be edited in Super Slow-mo player.
+
+## Samsung S20
+
+https://www.samsung.com/ch_fr/smartphones/galaxy-s20/specs/
+
+Slow Motion
+960fps @HD, 240fps @FHD
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,6 @@
 
 CAMERA SLOW MOTION CALIBRATION
 
-# DESCRIPTION OF THE PROJECT
-
-The goal of this project is to calibrate the slow motion frame rate of a smartphone.
-14 LEDs are displaying a counter that counts from 0 to 16383 in binary.
-The counter is updated at 1 ms interval with a timer.
-
-# SETUP
-
-This project works only with ATmega328P microcontrolers (ARDUINO UNO, Nano, Micro...).
-Connect the LEDs to output 0 to 13
-
-# ATmega328P datasheet
-
-http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
-
-# ARTICLE ABOUT TIMERS (in French)
-
-https://www.locoduino.org/spip.php?article84
-
-
-# Samsung S21
-
-https://www.androidpolice.com/2021/01/20/samsungs-smaller-cheaper-s21-and-s21-beat-the-ultra-at-slow-mo-video/
-
-https://www.samsung.com/global/galaxy/galaxy-s21-5g/specs/
-
-Super Slow-mo only supports HD reans. On Galaxy S21 5G and S21+ 5G, users can record approximately 0.5 seconds of video captured at 960 fps with approximately 16 seconds of playback. On Galaxy S21 Ultra 5G, users can record approximately 1 second of video captured at 480 fps and digitally enhance the video to 960 fps with approximately 32 seconds of playback. Playback time can be edited in Super Slow-mo player.
-
-# Samsung S20
-
-https://www.samsung.com/ch_fr/smartphones/galaxy-s20/specs/
-
-Slow Motion
-960fps @HD, 240fps @FHD
-
 */
 
 #include <Arduino.h>
@@ -135,14 +100,14 @@ void setup()
 
     /*
 
-   Initialize Timer1.
+    Initialize Timer1.
 
-   t = N / clkIO × ( 1 + OCR1A )
+    t = N / clkIO × ( 1 + OCR1A )
 
-   t: interval between each interrupt (s)
-   N: prescale factor (-)
-   clkIO: clock frequency (Hz), 16 MHz on regular UNO boards
-   OCR1A: output compare 1A used as TOP value
+    t: interval between each interrupt (s)
+    N: prescale factor (-)
+    clkIO: clock frequency (Hz), 16 MHz on regular UNO boards
+    OCR1A: output compare 1A used as TOP value
 
     for t = 1 s
     CS12 CS11 CS10 = 1 0 1 => prescale factor N = 1024
@@ -168,13 +133,18 @@ void setup()
     */
 
     cli();                      // Disable global interrupts.
+
     TCCR1A = 0b00000000;        // Set normal mode of operation.
     TCCR1B = 0b00000000;        //
+
     bitWrite(TCCR1B, WGM12, 1); // Turn on CTC mode.
+
     bitWrite(TCCR1B, CS12, 0);  // Prescale factor N.
-    bitWrite(TCCR1B, CS11, 0);  // See datasheet page 110
-    bitWrite(TCCR1B, CS10, 1);  // for values of CS10, CS11 and CS12.
+    bitWrite(TCCR1B, CS11, 0);  //      See datasheet page 110
+    bitWrite(TCCR1B, CS10, 1);  //      for values of CS10, CS11 and CS12.
+
     OCR1A = 16666;              // Set compare match register to desired timer count.
+
     TIMSK1 |= (1 << OCIE1A);    // Enable timer compare interrupt.
     sei();                      // Enable global interrupts.
 
